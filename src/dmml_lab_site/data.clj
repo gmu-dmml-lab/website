9055ef8034aaf31ddf651441776949c8e7fa9f0b
Matt Revelle <mrevelle@gmail.com>
2014-07-31 16:47:17 -0400

    Added images and updated profile data. Modified profile template to introduce whitespace if no interests are available. Fixes a problem with how profiles flow and which column they're placed.

--- src/dmml_lab_site/data.clj
+++ src/dmml_lab_site/data.clj
@@ -12,77 +12,57 @@
                       [{:homepage "http://cs.gmu.edu/~dbarbara/"
                         :name "Daniel Barbara"
                         :interests nil
-                        :email ""
                         :img "grey_square.jpg"}
                        {:homepage "http://cs.gmu.edu/~carlotta/students/"
                         :name "Carlotta Domeniconi"
                         :interests nil
-                        :email ""
                         :img "grey_square.jpg"}
                        {:homepage "http://www.cs.gmu.edu/~jessica/"
                         :name "Jessica Lin"
                         :interests nil
-                        :email ""
                         :img "grey_square.jpg"}
                        {:homepage "http://www.cs.gmu.edu/~hrangwal/"
                         :name "Huzefa Rangwala"
                         :interests nil
-                        :email ""
                         :img "grey_square.jpg"}]))
 
 (def phd-students (sort-by (comp last-name :name)
-                           [{:homepage "http://gmu.academia.edu/MattRevelle"
+                           [{:homepage "http://dynamictyping.com"
                              :name "Matt Revelle"
-                             :interests ["probabilistic models", "social networks"]
-                             :email ""
+                             :interests ["probabilistic models" "unsupervised learning" "social networks"]
                              :img "matt_revelle.jpg"}
                             {:homepage "#"
                              :name "David Etter"
                              :interests nil
-                             :email ""
-                             :img "grey_square.jpg"}
-                            {:homepage "#"
-                             :name "James Rogers"
-                             :interests nil
-                             :email ""
                              :img "grey_square.jpg"}
                             {:homepage "#"
                              :name "Rohan Khade"
                              :interests nil
-                             :email ""
                              :img "grey_square.jpg"}
-                            {:homepage "http://gmu.academia.edu/TanwisthaSaha"
+                            {:homepage "http://www.cs.gmu.edu/~tsaha/"
                              :name "Tanwistha Saha"
-                             :interests nil
-                             :email ""
-                             :img "grey_square.jpg"}
+                             :interests ["multi-label learning" "active learning"
+                                         "semi-supervised learning" "social networks"]
+                             :img "tanwistha_saha.jpg"}
                             {:homepage "http://mason.gmu.edu/~rotunba/"
                              :name "Rasaq Otunba"
                              :interests nil
-                             :email ""
-                             :img "grey_square.jpg"}
-                            {:homepage "#"
-                             :name "Loulwah S Al-Sumait"
-                             :interests nil
-                             :email ""
                              :img "grey_square.jpg"}
                             {:homepage "#"
                              :name "Xing Wang"
                              :interests nil
-                             :email ""
                              :img "grey_square.jpg"}
                             {:homepage "#"
                              :name "Anveshi Charuvaka"
                              :interests nil
-                             :email ""
                              :img "grey_square.jpg"}
-                            {:homepage "#"
+                            {:homepage "https://sites.google.com/site/azadnaik/"
                              :name "Azad Naik"
-                             :email ""
-                             :img "grey_square.jpg"}
+                             :interests ["multi-task learning" "hierarchical classification"]
+                             :img "azad_naik.jpg"}
                             {:homepage "#"
                              :name "Debdipto Misra"
-                             :email ""
+                             :interests nil
                              :img "grey_square.jpg"}]))
 
 (def ms-students (sort-by (comp last-name :name)
@@ -100,15 +80,35 @@
                                 []))
 
 (def alumni (sort-by :year >
-                     [{:homepage nil
+                     [{:homepage "#"
                        :name "Sam Blasiak"
                        :year 2013
                        :previous "PhD"
                        :img "grey_square.jpg"}
-                      {:homepage nil
+                      {:homepage "#"
                        :name "Yazhou Ren"
                        :year 2014
                        :previous "Visiting Scholar"
+                       :img "grey_square.jpg"}
+                      {:homepage "http://cs.gmu.edu/~pwang7/"
+                       :name "Pu Wang"
+                       :year 2011
+                       :previous "PhD"
+                       :img "grey_square.jpg"}
+                      {:homepage "#"
+                       :name "Loulwah AlSumait"
+                       :year 2009
+                       :previous "PhD"
+                       :img "grey_square.jpg"}
+                      {:homepage "http://mason.gmu.edu/~zrasheed/"
+                       :name "Zeehasham Rasheed"
+                       :year 2013
+                       :previous "PhD"
+                       :img "grey_square.jpg"}
+                      {:homepage "#"
+                       :name "James Rogers"
+                       :year 2010
+                       :previous "PhD"
                        :img "grey_square.jpg"}]))
 
 (def projects {})
