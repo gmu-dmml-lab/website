2d2fa3695ff20a53c5514d55d040e90a4516e302
Matt Revelle <mrevelle@gmail.com>
2014-07-31 17:01:45 -0400

    Added :interests? field to remove interests list in profiles for users with empty interests lists.

--- src/dmml_lab_site/data.clj
+++ src/dmml_lab_site/data.clj
@@ -7,109 +7,125 @@
   [name :- sc/Str]
   (-> name (str/split #"\s+") last))
 
+(sm/defn add-interests? :- {sc/Any sc/Any}
+  [profile :- {sc/Any sc/Any}]
+  (assoc profile :interests? (not (empty? (:interests profile)))))
+
 ;; TODO: data that can later be put in external files.
 (def faculty (sort-by (comp last-name :name)
-                      [{:homepage "http://cs.gmu.edu/~dbarbara/"
-                        :name "Daniel Barbara"
-                        :interests nil
-                        :img "grey_square.jpg"}
-                       {:homepage "http://cs.gmu.edu/~carlotta/students/"
-                        :name "Carlotta Domeniconi"
-                        :interests nil
-                        :img "grey_square.jpg"}
-                       {:homepage "http://www.cs.gmu.edu/~jessica/"
-                        :name "Jessica Lin"
-                        :interests nil
-                        :img "grey_square.jpg"}
-                       {:homepage "http://www.cs.gmu.edu/~hrangwal/"
-                        :name "Huzefa Rangwala"
-                        :interests nil
-                        :img "grey_square.jpg"}]))
+                      (->>
+                       [{:homepage "http://cs.gmu.edu/~dbarbara/"
+                         :name "Daniel Barbara"
+                         :interests nil
+                         :img "grey_square.jpg"}
+                        {:homepage "http://cs.gmu.edu/~carlotta/students/"
+                         :name "Carlotta Domeniconi"
+                         :interests nil
+                         :img "grey_square.jpg"}
+                        {:homepage "http://www.cs.gmu.edu/~jessica/"
+                         :name "Jessica Lin"
+                         :interests nil
+                         :img "grey_square.jpg"}
+                        {:homepage "http://www.cs.gmu.edu/~hrangwal/"
+                         :name "Huzefa Rangwala"
+                         :interests nil
+                         :img "grey_square.jpg"}]
+                       (map add-interests?))))
 
 (def phd-students (sort-by (comp last-name :name)
-                           [{:homepage "http://dynamictyping.com"
-                             :name "Matt Revelle"
-                             :interests ["probabilistic models" "unsupervised learning" "social networks"]
-                             :img "matt_revelle.jpg"}
-                            {:homepage "#"
-                             :name "David Etter"
-                             :interests nil
-                             :img "grey_square.jpg"}
-                            {:homepage "#"
-                             :name "Rohan Khade"
-                             :interests nil
-                             :img "grey_square.jpg"}
-                            {:homepage "http://www.cs.gmu.edu/~tsaha/"
-                             :name "Tanwistha Saha"
-                             :interests ["multi-label learning" "active learning"
-                                         "semi-supervised learning" "social networks"]
-                             :img "tanwistha_saha.jpg"}
-                            {:homepage "http://mason.gmu.edu/~rotunba/"
-                             :name "Rasaq Otunba"
-                             :interests nil
-                             :img "grey_square.jpg"}
-                            {:homepage "#"
-                             :name "Xing Wang"
-                             :interests nil
-                             :img "grey_square.jpg"}
-                            {:homepage "#"
-                             :name "Anveshi Charuvaka"
-                             :interests nil
-                             :img "grey_square.jpg"}
-                            {:homepage "https://sites.google.com/site/azadnaik/"
-                             :name "Azad Naik"
-                             :interests ["multi-task learning" "hierarchical classification"]
-                             :img "azad_naik.jpg"}
-                            {:homepage "#"
-                             :name "Debdipto Misra"
-                             :interests nil
-                             :img "grey_square.jpg"}]))
+                           (->>
+                            [{:homepage "http://dynamictyping.com"
+                              :name "Matt Revelle"
+                              :interests ["probabilistic models" "unsupervised learning" "social networks"]
+                              :img "matt_revelle.jpg"}
+                             {:homepage "#"
+                              :name "David Etter"
+                              :interests nil
+                              :img "grey_square.jpg"}
+                             {:homepage "#"
+                              :name "Rohan Khade"
+                              :interests nil
+                              :img "grey_square.jpg"}
+                             {:homepage "http://www.cs.gmu.edu/~tsaha/"
+                              :name "Tanwistha Saha"
+                              :interests ["multi-label learning" "active learning"
+                                          "semi-supervised learning" "social networks"]
+                              :img "tanwistha_saha.jpg"}
+                             {:homepage "http://mason.gmu.edu/~rotunba/"
+                              :name "Rasaq Otunba"
+                              :interests nil
+                              :img "grey_square.jpg"}
+                             {:homepage "#"
+                              :name "Xing Wang"
+                              :interests nil
+                              :img "grey_square.jpg"}
+                             {:homepage "#"
+                              :name "Anveshi Charuvaka"
+                              :interests nil
+                              :img "grey_square.jpg"}
+                             {:homepage "https://sites.google.com/site/azadnaik/"
+                              :name "Azad Naik"
+                              :interests ["multi-task learning" "hierarchical classification"]
+                              :img "azad_naik.jpg"}
+                             {:homepage "#"
+                              :name "Debdipto Misra"
+                              :interests nil
+                              :img "grey_square.jpg"}]
+                            (map add-interests?))))
 
 (def ms-students (sort-by (comp last-name :name)
-                          [{:homepage "#"
-                            :name "Nikhil Muralidhar"
-                            :email ""
-                            :img "grey_square.jpg"}]))
+                          (->>
+                           [{:homepage "#"
+                             :name "Nikhil Muralidhar"
+                             :email ""
+                             :img "grey_square.jpg"}]
+                           (map add-interests?))))
 (def ug-students (sort-by (comp last-name :name)
-                          [{:homepage "#"
-                            :name "Mackenzie Sweeney"
-                            :email ""
-                            :img "grey_square.jpg"}]))
+                          (->>
+                           [{:homepage "#"
+                             :name "Mackenzie Sweeney"
+                             :email ""
+                             :img "grey_square.jpg"}]
+                           (map add-interests?))))
 
 (def visiting-scholars (sort-by (comp last-name :name)
-                                []))
+                                (->>
+                                 []
+                                 (map add-interests?))))
 
 (def alumni (sort-by :year >
-                     [{:homepage "#"
-                       :name "Sam Blasiak"
-                       :year 2013
-                       :previous "PhD"
-                       :img "grey_square.jpg"}
-                      {:homepage "#"
-                       :name "Yazhou Ren"
-                       :year 2014
-                       :previous "Visiting Scholar"
-                       :img "grey_square.jpg"}
-                      {:homepage "http://cs.gmu.edu/~pwang7/"
-                       :name "Pu Wang"
-                       :year 2011
-                       :previous "PhD"
-                       :img "grey_square.jpg"}
-                      {:homepage "#"
-                       :name "Loulwah AlSumait"
-                       :year 2009
-                       :previous "PhD"
-                       :img "grey_square.jpg"}
-                      {:homepage "http://mason.gmu.edu/~zrasheed/"
-                       :name "Zeehasham Rasheed"
-                       :year 2013
-                       :previous "PhD"
-                       :img "grey_square.jpg"}
-                      {:homepage "#"
-                       :name "James Rogers"
-                       :year 2010
-                       :previous "PhD"
-                       :img "grey_square.jpg"}]))
+                     (->>
+                      [{:homepage "#"
+                        :name "Sam Blasiak"
+                        :year 2013
+                        :previous "PhD"
+                        :img "grey_square.jpg"}
+                       {:homepage "#"
+                        :name "Yazhou Ren"
+                        :year 2014
+                        :previous "Visiting Scholar"
+                        :img "grey_square.jpg"}
+                       {:homepage "http://cs.gmu.edu/~pwang7/"
+                        :name "Pu Wang"
+                        :year 2011
+                        :previous "PhD"
+                        :img "grey_square.jpg"}
+                       {:homepage "#"
+                        :name "Loulwah AlSumait"
+                        :year 2009
+                        :previous "PhD"
+                        :img "grey_square.jpg"}
+                       {:homepage "http://mason.gmu.edu/~zrasheed/"
+                        :name "Zeehasham Rasheed"
+                        :year 2013
+                        :previous "PhD"
+                        :img "grey_square.jpg"}
+                       {:homepage "#"
+                        :name "James Rogers"
+                        :year 2010
+                        :previous "PhD"
+                        :img "grey_square.jpg"}]
+                      (map add-interests?))))
 
 (def projects {})
 (def publications {})
